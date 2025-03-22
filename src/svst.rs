908a98a9be2e0a7c7c90061467ed742e808ac690
Marián Konček <marian.koncek@mailbox.org>
2025-03-22 23:01:06 +0100

    Add default comparator for aliases

--- src/svst.rs
+++ src/svst.rs
@@ -9,5 +9,5 @@ mod vector_storage;
 mod bit_indexing;
 
 pub type SVec<Type, const SIZE: usize> = svec::SVec<Type, SIZE>;
-pub type AATreeSet<KeyType> = aa::Set<KeyType>;
-pub type AATreeMap<KeyType, MappedType> = aa::Map<KeyType, MappedType>;
+pub type AATreeSet<KeyType, Compare = crate::DefaultComparator> = aa::Set<KeyType, Compare>;
+pub type AATreeMap<KeyType, MappedType, Compare = crate::DefaultComparator> = aa::Map<KeyType, MappedType, Compare>;
