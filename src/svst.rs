b2984d04ff0cd37b38baa389a108caff8d40811f
Marián Konček <marian.koncek@mailbox.org>
2025-03-22 22:46:23 +0100

    Add public alias for SVec

--- src/svst.rs
+++ src/svst.rs
@@ -8,5 +8,6 @@ mod repository;
 mod vector_storage;
 mod bit_indexing;
 
+pub type SVec<Type, const SIZE: usize> = svec::SVec<Type, SIZE>;
 pub type AATreeSet<KeyType> = aa::Set<KeyType>;
 pub type AATreeMap<KeyType, MappedType> = aa::Map<KeyType, MappedType>;
